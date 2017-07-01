1d7784489eb3c3f66405a4f97d1c0e53c68613e8
Sharvani Haran <sharvaniharan@yahoo.com>
2017-06-30 20:47:48 -0700

    Added Search feature - Text only
    - Added Edit text with a continuous text change listener
    - Added intuitive UI for searching
    - Code structure organized with a facade design to provide MainActivity with only the portion of the TextWatcher Callback that is needed.

---
+++ app/src/main/java/com/androiddev/sharvani/wikiimagesearch/views/search_edittext/TextListener.java
@@ -0,0 +1,9 @@
+package com.androiddev.sharvani.wikiimagesearch.views.search_edittext;
+
+/**
+ * Created by Sharvani on 6/30/17.
+ */
+
+public interface TextListener {
+    public void getCurrentText(String currentText);
+}
